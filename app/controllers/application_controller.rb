a0a53c15da7771b2fc6627f20db4604b42f56622
Alex Yang <mdalexinca@gmail.com>
2017-05-31 21:05:32 +0000

    OM

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   end 
   
   def welcome
-    render html: "Welcome to Oddity Market!"
+    render html: "Welcome to the Oddity Market!"
   end
 end
