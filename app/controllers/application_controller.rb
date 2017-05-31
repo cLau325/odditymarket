ffbec492bb121979606df09d27825b48a7df6a46
Alex Yang <mdalexinca@gmail.com>
2017-05-31 20:53:05 +0000

    Welcome To OM

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base 
   protect_from_forgery with: :exception
   def hello
-    render html: "hello, world!"
+    render html: "Hello World!"
   end 
+  
+  def welcome
+    render html: "Welcome to Oddity Market!"
+  end
 end
