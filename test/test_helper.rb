6e57647d8eedc64c747ad6460dbfc6b018a1987d
Hiu Ching Lau <hclau25@gmail.com>
2017-06-11 00:46:35 +0000

    past all test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,27 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
-<<<<<<< HEAD
-<<<<<<< HEAD
-    require File.expand_path('../../config/environment', __FILE__)
-    require 'rails/test_help'
-    require "minitest/reporters"
-    Minitest::Reporters.use!
-    
-=======
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
->>>>>>> filling-in-layout
-=======
-
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
-require "minitest/reporters"
-
-Minitest::Reporters.use!
->>>>>>> b5afeaf916255ae049f5877d310618cbf4757276
 class ActiveSupport::TestCase
-# Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order. fixtures :all
-      # Add more helper methods to be used by all tests here...
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+  # Add more helper methods to be used by all tests here...
 end
