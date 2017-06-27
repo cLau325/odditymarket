7344d64dd19309a8db1b7df79f907234458db097
Hiu Ching Lau <hclau25@gmail.com>
2017-06-27 23:48:29 +0000

    fixed flash block error

--- db/schema.rb
+++ db/schema.rb
@@ -51,9 +51,9 @@ ActiveRecord::Schema.define(version: 20170627062741) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "password_digest"
   end
 
 end
