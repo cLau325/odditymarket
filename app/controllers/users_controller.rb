b6bbc0cdf1ea5c094cd470cca5d620804f5ccbe4
Hiu Ching Lau <hclau25@gmail.com>
2017-06-27 23:36:07 +0000

    redirect page to user home page if successfully create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) 
     if @user.save   # 处理注册成功的情况 Create Sucessful 
-      redirect_to @user
+      redirect_to user_url(@user)
     else# 处理注册失敗的情况 Create Failed
       render 'new' 
     end
