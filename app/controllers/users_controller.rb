b52990d73c430b1cdec8c62ee40e059e8ae2b395
Hiu Ching Lau <hclau25@gmail.com>
2017-06-27 23:40:16 +0000

    Added welcoming message when new user is crested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params) 
     if @user.save   # 处理注册成功的情况 Create Sucessful 
+      flash[:success] = "Welcome to the Oddity Market"
       redirect_to user_url(@user)
     else# 处理注册失敗的情况 Create Failed
       render 'new' 
