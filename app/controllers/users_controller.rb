a2e53f69fadfacb41a2a6bab8344ccfb6a0b7d7c
Hiu Ching Lau <hclau25@gmail.com>
2017-06-27 23:31:39 +0000

    Added the failed page for sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params) 
+    if @user.save   # 处理注册成功的情况 Create Sucessful 
+      redirect_to @user
+    else# 处理注册失敗的情况 Create Failed
+      render 'new' 
+    end
+  end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   resources :disprods
   resources :forsales
   resources :forauctions
- 
+  resources :users 
+
   root 'static_pages#home'
   get 'users/new'
   get '/about', to: 'static_pages#about' 
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/display', to: 'disprods#index'
   get '/forauction', to: 'forauctions#index'
   get '/forsale', to: 'forsales#index'
