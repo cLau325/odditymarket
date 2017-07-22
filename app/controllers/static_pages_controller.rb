8d7d60d0eb2de1279851c763d78d0fffc09d1a1f
Yeh Cheng Ho <maxtonys@hotmail.com>
2017-07-22 19:52:16 +0000

    modify home_recent div container

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @disprod = Disprod.includes(@disprod).limit(3).offset(0)
-    @forauction = Forauction.includes(@forauction).limit(3).offset(0)
-    @forsale = Forsale.includes(@forsale).limit(3).offset(0)
+    @disprod = Disprod.includes(@disprod).order("created_at desc").limit(3).offset(0)
+    @forauction = Forauction.includes(@forauction).order("created_at desc").limit(3).offset(0)
+    @forsale = Forsale.includes(@forsale).limit(3).order("created_at desc").offset(0)
     @user = Forsale.includes(@user)
   end
 
